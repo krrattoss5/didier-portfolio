63c7bb490284b714430f3c49a681621c40ff9618
krrattoss5 <krrattoss5@gmail.com>
2024-05-16 18:44:55 -0500

    (finished/Services.tsx)

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,4 +1,4 @@
-import { CodeBracketSquareIcon } from '@heroicons/react/20/solid'
+import { CodeBracketSquareIcon, CommandLineIcon, RocketLaunchIcon } from '@heroicons/react/20/solid'
 import React from 'react'
 
 const Services = () => {
@@ -18,19 +18,19 @@ const Services = () => {
           </div>
         </div>
         <div>
-          <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'>
-            <CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
+          <div className='bg-orange-700 hover:scale-110 transform transition-all duration-300 uppercase font-semibold text-center p-[2rem]'>
+            <RocketLaunchIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
             <h2 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
-              Frontend
+              Backend
             </h2>
             <p className='text-[15px] text-[#d3d2d2] font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla mollitia culpa, alias, dolores laboriosam ipsam necessitatibus aliquam doloremque nesciunt cupiditate sed similique hic atque voluptatum fugiat praesentium voluptatibus, veritatis ullam.</p>
           </div>
         </div>
         <div>
-          <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'>
-            <CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
+          <div className='bg-blue-700 hover:scale-110 transform transition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]'>
+            <CommandLineIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
             <h2 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
-              Frontend
+              FullStack
             </h2>
             <p className='text-[15px] text-[#d3d2d2] font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla mollitia culpa, alias, dolores laboriosam ipsam necessitatibus aliquam doloremque nesciunt cupiditate sed similique hic atque voluptatum fugiat praesentium voluptatibus, veritatis ullam.</p>
           </div>
