dbf6ea58ce04667abde5f7207f5c1f747140b5ab
krrattoss5 <krrattoss5@gmail.com>
2024-05-15 20:32:34 -0500

    (add/h1: hi, i`m didier!)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,6 +6,9 @@ const Hero = () => {
     <div className='h-[88vh] bg-[url("/images/banner.jpg")] bg-cover bg-center'>
       <Particle />
       <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
+        <h1 className='text-[35px] md:text-[50px] text-white font-bold'>
+          HI, I`M <span className='text-yellow-400'>DIDIER!</span>
+        </h1>
       </div>
     </div>
   )
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -10,7 +10,7 @@ const Nav:React.FC<Props> = ({handleOpenMenu}) => {
     <div className='w-[100%] sticky top-0 h-[12vh] bg-[#141c27] shadow-md'>
       <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
         <h1 className='flex-[0.6] cursor-pointer text-[25px] text-white font-bold'>
-          DIDIER
+          PORTFOLIO
           <span className='text-yellow-300'>DEV</span>
         </h1>
         <div className='nav-link'>Home</div>
