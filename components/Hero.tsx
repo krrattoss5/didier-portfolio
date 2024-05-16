8ec8fc77e92186dd7868183491249e7847bf9edc
krrattoss5 <krrattoss5@gmail.com>
2024-05-15 22:43:50 -0500

    (finished/Hero)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Particle from './Particle'
 import TextEffect from './TextEffect'
 import Image from 'next/image'
+import { ArrowDownTrayIcon } from '@heroicons/react/20/solid'
 
 const Hero = () => {
   return (
@@ -13,7 +14,13 @@ const Hero = () => {
             HI, I`M <span className='text-yellow-400'>DIDIER!</span>
           </h1>
           <TextEffect />
-          <p className='mt-[2rem] text-[20px] text-[#ffffff92]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur provident velit vitae atque dolore, non blanditiis explicabo reiciendis cupiditate quas adipisci quibusdam eaque vel deleniti fugiat voluptatibus modi repellat aperiam!</p>
+          <p className='mt-[1.5rem] text-[18px] text-[#ffffff92]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur provident velit vitae atque dolore, non blanditiis explicabo reiciendis cupiditate quas adipisci quibusdam eaque vel deleniti fugiat voluptatibus modi repellat aperiam!</p>
+          <div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
+            <button className='px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2'>
+              <p>Download Cv</p>
+              <ArrowDownTrayIcon className='w-[1.6rem] h-[1.7rem] text-black' />
+            </button>
+          </div>
         </div>
         <div className='w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]'>
           <Image src='/images/u1.jpg' alt='user' layout='fill' className='object-cover rounded-full' />
