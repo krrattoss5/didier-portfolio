ddf6f1c67e7516deead90eb56b7cb4e98b42f075
krrattoss5 <krrattoss5@gmail.com>
2024-05-16 16:25:56 -0500

    (add/servicesCards)

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,3 +1,4 @@
+import { CodeBracketSquareIcon } from '@heroicons/react/20/solid'
 import React from 'react'
 
 const Services = () => {
@@ -6,6 +7,35 @@ const Services = () => {
       <p className='heading'>
         My, <span className='text-yellow-400'>Services</span>
       </p>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white'>
+        <div>
+          <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'>
+            <CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
+            <h2 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
+              Frontend
+            </h2>
+            <p className='text-[15px] text-[#d3d2d2] font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla mollitia culpa, alias, dolores laboriosam ipsam necessitatibus aliquam doloremque nesciunt cupiditate sed similique hic atque voluptatum fugiat praesentium voluptatibus, veritatis ullam.</p>
+          </div>
+        </div>
+        <div>
+          <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'>
+            <CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
+            <h2 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
+              Frontend
+            </h2>
+            <p className='text-[15px] text-[#d3d2d2] font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla mollitia culpa, alias, dolores laboriosam ipsam necessitatibus aliquam doloremque nesciunt cupiditate sed similique hic atque voluptatum fugiat praesentium voluptatibus, veritatis ullam.</p>
+          </div>
+        </div>
+        <div>
+          <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'>
+            <CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
+            <h2 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
+              Frontend
+            </h2>
+            <p className='text-[15px] text-[#d3d2d2] font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla mollitia culpa, alias, dolores laboriosam ipsam necessitatibus aliquam doloremque nesciunt cupiditate sed similique hic atque voluptatum fugiat praesentium voluptatibus, veritatis ullam.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
