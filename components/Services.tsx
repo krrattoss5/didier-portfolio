04f537fa4127b85cde7c97f35d7929e38eaac377
krrattoss5 <krrattoss5@gmail.com>
2024-05-16 15:57:19 -0500

    (add/heading styles)

--- components/Services.tsx
+++ components/Services.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]'>
+      <p className='heading'>
+        My, <span className='text-yellow-400'>Services</span>
+      </p>
+    </div>
   )
 }
 
