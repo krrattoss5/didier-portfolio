975e80919129c0587391b69b1ea4e288a8a93a61
krrattoss5 <krrattoss5@gmail.com>
2024-05-17 23:46:55 -0500

    (add/TestimonialSlider)

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <div className='bg-[#02050a] pt-[4rem] md:pt-[8rem]'>
+      <h1 className='heading'>
+        Client <span className='text-yellow-400'>Review</span>
+      </h1>
+      <div className='pt-[5rem] pb-[4rem] w-[80%] mx-auto'>
+      </div>
+    </div>
   )
 }
 
