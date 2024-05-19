de12907b3e57279484f0a626a5ed028f7431c94a
krrattoss5 <krrattoss5@gmail.com>
2024-05-19 18:04:30 -0500

    (finished/footer)

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='pt-[8rem] pb-[4rem] bg-[#02050a]'>
-      <div className='border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-[80%] mx-auto gap-[3rem]'>
+      <div className='border-b-[1px] pb-[6rem] border-gray-400 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-[80%] mx-auto gap-[3rem]'>
         <div className='flex items-center space-x-6 '>
           <div className='md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]'>
             <MapIcon className='md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] text-black'/>
@@ -34,14 +34,14 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className='w-[80%]mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between'>
-        <div className="text-[18px] mb-[2rem] md:mb-0 text-white opacity-20">
+      <div className='w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between'>
+        <div className="text-[16px] mb-[2rem] md:mb-0 text-white opacity-20 hover:opacity-100">
           React Developer Didier Pereira 2024 | Todos los derechos reservados.
         </div>
-        <div className='flex items-center space-x10'>
-          <p className='text-[18px] text-white opacity-20'>Terms & Conditions</p>
-          <p className='text-[18px] text-white opacity-20'>Privacy Policy</p>
-          <p className='text-[18px] text-white opacity-20'></p>
+        <div className='flex items-center space-x-10'>
+          <p className='text-[16px] text-white opacity-20 hover:opacity-100'>Terms & Conditions</p>
+          <p className='text-[16px] text-white opacity-20 hover:opacity-100'>Privacy Policy</p>
+          <p className='text-[16px] text-white opacity-20 hover:opacity-100'>Sitemap</p>
         </div>
       </div>
     </div>
