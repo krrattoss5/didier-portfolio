7487572c64da866d1c01a4c70c965ab7e1c90103
krrattoss5 <krrattoss5@gmail.com>
2024-06-20 20:36:23 -0500

    (refact translated titles to spanish git push )

--- components/Services.tsx
+++ components/Services.tsx
@@ -9,7 +9,7 @@ const Services = () => {
   return (
     <div className="bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]">
       <p data-aos="zoom-in-down" className="heading">
-        My, <span className="text-yellow-400">Services</span>
+        Mis, <span className="text-yellow-400">Servicios</span>
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white">
         <div data-aos="flip-left">
