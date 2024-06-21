2fee091d9c2392d1604cca989471a19aa2b9df5f
krrattoss5 <krrattoss5@gmail.com>
2024-06-20 20:34:27 -0500

    (translated menu titles to spanish)

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -13,12 +13,12 @@ const MobileNav = ({handleOpenMenu, isOpenMenu}:Props) => {
   return (
     <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-[#09101a]`}>
       <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
-        <div className='nav-link-mobile'>Home</div>
-        <div className='nav-link-mobile'>SERVICES</div>
-        <div className='nav-link-mobile'>ABOUT</div>
-        <div className='nav-link-mobile'>PROJECTS</div>
-        <div className='nav-link-mobile'>BLOG</div>
-        <div className='nav-link-mobile'>CONTACT</div>
+        <div className='nav-link-mobile'>INICIO</div>
+        <div className='nav-link-mobile'>SERVICIOS</div>
+        <div className='nav-link-mobile'>SOBRE MI</div>
+        <div className='nav-link-mobile'>PROYECTOS</div>
+        {/* <div className='nav-link-mobile'>BLOG</div> */}
+        <div className='nav-link-mobile'>CONTACTO</div>
       </div>
       <div
         onClick={handleOpenMenu}
